6c1d858e1cffa36897782a61c73ba69c3393bd4c
aleu0007 <aleu0007@student.monash.edu>
2024-10-15 21:52:13 +1100

    content for diet section

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,35 @@
-// js/navbar.js
-
 function changeContent(section) {
   const contentMap = {
     section1: {
       title: "Cost of an Energy Sufficient Meal",
-      text: "Content for Section 1 goes here.",
+      cost: "$0.83",
+      text: `Global average calculated using the cheapest starchy staple available multiplied
+      by quantities needed for 2,330 calories a day. 
+      <br> <br>
+      People who cannot afford energy sufficient meals are concentrated in <strong>low-income countries</strong>.`,
       chart: "js/scatter_energy_sufficient_price.json"
     },
     section2: {
       title: "Cost of a Nutrient Balanced Meal",
-      text: "Content for Section 2 goes here.",
+      cost: "$2.46",
+      text: `Global average determined by adding together the most affordable selection of food items 
+        that satisfy the energy and intake needs for 23 essential macro and micronutrients. 
+        <br> <br>
+        In low-income countries, more than <strong>three quarters</strong> of the population cannot afford a nutrient adequate meal. <br>
+        In middle-income countries, more than <strong>half</strong> cannot afford it, but in high-income countries, only less than 1 percent cannot afford a nutrient adequate meal.`,
       chart: "js/scatter_nutrient_sufficient_price.json"
     },
     section3: {
       title: "Cost of a Healthy Meal",
-      text: "Content for Section 3 goes here.",
+      cost: "$3.31",
+      text: `Global average calculated by meeting dietary requirements to protect long term health and prevent malnutrition.
+        <br> <br>
+        <strong>90%</strong> of low income countries population cannot afford a healthy meal.
+        <strong>70%</strong> of middle income countries population cannot afford a healthy meal.
+        <br> <br>
+        Costs <strong> 1.3 times </strong> more than a nutrient balanced meal
+        <br>
+        Costs <strong> 4 times </strong> more than an energy sufficient meal`,
       chart: "js/scatter_healthy_meal_price.json"
     }
   };
@@ -22,7 +37,8 @@ function changeContent(section) {
   // Check if the section exists in contentMap
   if (contentMap[section]) {
     document.getElementById('content-title').innerText = contentMap[section].title;
-    document.getElementById('content-text').innerText = contentMap[section].text;
+    document.getElementById('content-cost').innerText = contentMap[section].cost;
+    document.getElementById('content-text').innerHTML = contentMap[section].text; // Change to innerHTML
 
     // Clear previous chart
     document.getElementById('dynamic-chart').innerHTML = '';
